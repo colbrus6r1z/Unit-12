8f767c3fc69453e40b5642bc38cc1587f389a4ab
colbrus6r1z <russell91c@gmail.com>
2025-07-08 13:53:49 -0400

    Final Commit
    Errors handled.
    Changed names back to default suggested names.

--- Shadow_Sleuths-Detective.js
+++ Shadow_Sleuths-Detective.js
@@ -1,38 +1,35 @@
-function mysteryOperation ()
-{
-	const outcome = Math.random(); // Generates a random number between 0 and 1.
-
-	if (outcome < 0.5)
-	{let i 
-		console.log("The operation is completed successfully!");
-	}
-	else
-	{
-		throw new Error("The operation is failed mysteriously!");
-	}
-}
-const operationAttempts = 20;  
+const numberOfOperations = 20;	//Moved declaration of variables to the beginning of file.
 
 const daysOnSuccess = 13;
 const daysOnFailure = 1;
 const daysOnAttendance = 3;
 
-for(let i = 0; i < operationAttempts; i++ )
-{
-	try{
-		mysteryOperation();
-		daysAccrued += daysOnSuccess;
+let daysEarned = 0;
 
+function mysteryOperation() {
+	const outcome = Math.random(); // Generates a random number between 0 and 1.
+	if (outcome < 0.5) {
+		console.log("The operation is completed successfully!");
 	}
-	catch (error)
-	{
-		mysteryOperation();
-		daysAccrued += daysOnFailure;
-	}
-	finally{
-		mysteryOperation();
-		daysAccrued += daysOnAttendance
+	else {
+		throw new Error("The operation is failed mysteriously!");
 	}
 }
 
-console.log(daysAccrued);
+for (let i = 0; i < numberOfOperations; i++) {
+	try {
+		mysteryOperation();
+		daysEarned += daysOnSuccess;
+
+	}
+	catch (error) {
+		mysteryOperation();
+		daysEarned += daysOnFailure;
+	}
+	finally {
+		mysteryOperation();
+		daysEarned += daysOnAttendance
+	}
+}
+
+console.log(daysEarned);
